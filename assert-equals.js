c7dd3ccfa5111c1d389d862522fe55e64cd2d1ce
ShaughnAnderson94 <andersji94@gmail.com>
2023-07-18 16:28:41 +0100

    added code to compare simple numbers and strings

--- assert-equals.js
+++ assert-equals.js
@@ -1,8 +1,25 @@
 function assertEquals(expect, actual) {
-    if(typeof expect !== typeof actual){
-        console.log(`Expected type ${typeof expect} but found type ${typeof actual}`);
+   try { if(typeof expect !== typeof actual){
+        throw new Error(`Expected type ${typeof expect} but found type ${typeof actual}`);
+    }
+    else{
+if(expect !== actual){
+    throw new Error(`Expected ${expect} but found ${actual}`);
+}
     }
 }
-assertEquals(1, '1');
+    catch (error) {
+        // Handle the error
+        console.error("error:", error.message);
+      }
+}
+// test for detecting unequal type 
+// assertEquals(1, '1');
+
+// test for detecting unequal strings or numbers
+// assertEquals(1,2);
+// assertEquals('USA', 'UK');
+
+
 
 module.exports = assertEquals
