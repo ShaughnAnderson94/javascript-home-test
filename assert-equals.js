b036bfbbda641dcf3e2367eb49b4701ce8b28997
ShaughnAnderson94 <andersji94@gmail.com>
2023-07-18 15:54:49 +0100

    throw error for type missmatch

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,8 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+    if(typeof expect !== typeof actual){
+        console.log(`Expected type ${typeof expect} but found type ${typeof actual}`);
+    }
+}
+assertEquals(1, '1');
 
 module.exports = assertEquals
