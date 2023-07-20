888479f063978cbca1aa9c9e5dd6f22db0100526
ShaughnAnderson94 <andersji94@gmail.com>
2023-07-20 16:29:24 +0100

    array tests

--- assert-equals.js
+++ assert-equals.js
@@ -3,11 +3,24 @@ function assertEquals(expect, actual) {
         throw new Error(`Expected type ${typeof expect} but found type ${typeof actual}`);
     }
     else{
+        if(Array.isArray(expect) === true && expect.length !== actual.length){
+        throw new Error(`Expected array length ${expect.length} but found ${actual.length}`);
+        }
+        else{
+            if(Array.isArray(expect)){
+                actual.forEach((element, index) => {
+              if(element !== expect[index]){
+
+                throw new Error(`Expected ${expect[index]} but found ${element}`);
+              }       
+                });
+            }
 if(expect !== actual){
     throw new Error(`Expected ${expect} but found ${actual}`);
 }
     }
 }
+   }
     catch (error) {
         // Handle the error
         console.error("error:", error.message);
@@ -19,6 +32,13 @@ if(expect !== actual){
 // test for detecting unequal strings or numbers
 // assertEquals(1,2);
 // assertEquals('USA', 'UK');
+
+// test for array length difference
+// assertEquals([1,1,1],[1,1,1,1,1])
+
+// test for arrays with different values
+// assertEquals(['a','d'], ['a','b']);
+
 
 
 
